d5fecf6f0cdfb8e4c4971f07efc0a73e938340c1
uvmhe <uvmhectorcorrales@gmail.com>
2024-02-10 10:09:55 -0600

    Se modico archivo index.php y se agrego un archivo llamado contacto.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <h1>Esto es el título</h1>
 <span>Hola Mundo</span> skalsklaksal
+
+<body>Buenos días, Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit, esse nostrum, molestiae dolorum sed sint, fugiat vero in repellat aliquid ipsam neque dolor temporibus obcaecati veritatis vitae illum accusamus?</body>
